b473d69c200be99b434ad1fa391b45a1a3915b7b
CB7D <cb7d23@gmail.com>
2022-08-28 23:13:27 +0800

    feat: empty page

---
+++ src/Forest.vue
@@ -0,0 +1,29 @@
+<!-- https://www.jianshu.com/p/91328e6934c9 -->
+<!-- https://zhuanlan.zhihu.com/p/28967069 -->
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+const date = ref("Date")
+const getNowTime = () => {
+    const now = new Date()
+    const nowStr = now.toDateString()
+    date.value = nowStr
+}
+onMounted(() => {
+    setInterval(() => {
+        getNowTime()
+    }, 1000)
+});
+</script>
+
+<template>
+    <header>
+        {{ date }}
+    </header>
+    <body>
+        007 No Time To Die
+    </body>
+</template>
+
+<style>
+</style>
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 
 import App from "./App.vue";
+import Forest from "./Forest.vue";
 import router from "./router";
 
-import "./assets/main.css";
+// import "./assets/main.css";
+import "./assets/Forest.css";
 
-const app = createApp(App);
+// const app = createApp(App);
+const app = createApp(Forest);
 
 app.use(createPinia());
 app.use(router);
